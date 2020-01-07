0d0153165547f349d97792c88062ba641b520509
Jakub <jakub.suchenek.25@gmail.com>
2020-01-07 18:54:16 +0100

    Added more "about" content

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,13 +2,16 @@
   <div class="row">
     <div class="col-12">
       <p class="h1">What do you want to know?</p>
-    </div>
-    <div class="col-12">
       <p>
-        <span>Change colors:</span>
-        <button type="buton" @click="changeColors">Click</button>
+        Hello, my name's Jakub. I'm C++/Python programmer and web developer.
+        <br />One day I&nbsp;though: "I&nbsp;have a&nbsp;lot of&nbsp;avatars downloaded,
+        also I&nbsp;have no&nbsp;idea what to do..." and&nbsp;this is&nbsp;how
+        Avatar&nbsp;Database has&nbsp;been created.
       </p>
-      <p>Current theme: {{ currentTheme }}</p>
+      <p>
+        Make sure to visit
+        <a href="https://github.com/AnonymousX86">my GitHub profile</a> for more projects.
+      </p>
     </div>
   </div>
 </template>
@@ -18,23 +21,13 @@ export default {
   name: "about-page",
   data() {
     return {
-      title: "About",
-      theme: "light"
+      title: "About"
     };
   },
 
-  methods: {
-    changeColors() {
-      if (this.theme === "dark") this.theme = "light";
-      else this.theme = "dark";
-    }
-  },
+  methods: {},
 
-  computed: {
-    currentTheme() {
-      return this.theme;
-    }
-  }
+  computed: {}
 };
 </script>
 
