23355ec2f3b64d3839329e00e7183b6928d94760
Jakub <jakub.suchenek.25@gmail.com>
2020-01-21 22:31:56 +0100

    Removed "Contact" path

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import VueRouter from 'vue-router';
 
 import Home from './components/Home.vue';
 import About from './components/About.vue';
-import Contact from './components/Contact.vue';
 
 import Avatars from './components/Avatars.vue';
 
@@ -26,7 +25,6 @@ window.$ = window.jQuery = require('jquery');
 const routes = [
     { path: '/', name: 'home', component: Home },
     { path: '/about', name: 'about', component: About },
-    { path: '/contact', name: 'contact', component: Contact },
 ];
 
 const router = new VueRouter({
