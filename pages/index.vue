7ed21bf276ed4965f17d52d162dadf67ab006eb3
AnonymousX86 <jakub.suchenek.25@gmail.com>
2021-03-29 00:59:47 +0200

    Fix template & add Avatars component

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <template>
-  <div>
-    <b-row>
-      <b-col cols="12">
-        <h1>Avatar database</h1>
-        <h5 class="border-left pl-3">A lot of avatars, free to use</h5>
-        <p>This is currently empty homepage, everything will be added soon.</p>
-      </b-col>
-    </b-row>
-  </div>
+  <b-row no-gutters>
+    <b-col cols="12">
+      <h1>Avatar database</h1>
+      <h5 class="border-left pl-3">A lot of avatars, free to use</h5>
+      <p>This is currently empty homepage, everything will be added soon.</p>
+    </b-col>
+    <b-col cols="12">
+      <Avatars />
+    </b-col>
+  </b-row>
 </template>
 
 <script>
