e2e2ef77c54916ee701db88fce3e7680971bd129
AnonymousX86 <jakub.suchenek.25@gmail.com>
2021-03-28 01:07:53 +0100

    Remove unused styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,14 +27,4 @@ html {
   box-sizing: border-box;
   margin: 0;
 }
-
-/*.fade-enter-active,*/
-/*.fade-leave-active {*/
-/*  transition: opacity 0.5s ease;*/
-/*}*/
-
-/*.fade-enter-from,*/
-/*.fade-leave-to {*/
-/*  opacity: 0;*/
-/*}*/
 </style>
