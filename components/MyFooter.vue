2610f5a8b43966db17a0a68b6f0c53a8f40aa0fe
AnonymousX86 <jakub.suchenek.25@gmail.com>
2021-03-30 00:39:20 +0200

    Update footer

--- components/MyFooter.vue
+++ components/MyFooter.vue
@@ -1,15 +1,32 @@
 <template>
   <footer>
-    <p class="text-right text-muted pr-5">
-      Made by Jakub Suchenek |
-      <a href="https://github.com/AnonymousX86" title="Check my GitHub">
-        GitHub
-      </a>
-    </p>
+    <b-row no-gutters class="pb-5 pt-3 mx-5 border-top">
+      <b-col>
+        <a href="https://ko-fi.com/O5O542WQX" target="_blank" rel="noopener">
+          <b-img
+            height="36"
+            style="border: 0; height: 36px"
+            src="https://cdn.ko-fi.com/cdn/kofi1.png?v=2"
+            alt="Buy Me a Coffee at ko-fi.com"
+            class="d-inline"
+            fluid
+          />
+        </a>
+      </b-col>
+      <b-col>
+        <p class="text-right text-muted">
+          Made by Jakub Suchenek |
+          <a href="https://github.com/AnonymousX86" title="Check my GitHub">
+            GitHub
+          </a>
+        </p>
+      </b-col>
+    </b-row>
   </footer>
 </template>
 
 <script>
+// noinspection JSUnusedGlobalSymbols
 export default {
   name: "MyFooter",
 }
