6d984e751980806b97534fbe1c08e4c65be2ec7d
AnonymousX86 <jakub.suchenek.25@gmail.com>
2021-04-01 01:56:25 +0200

    Add Ko-fi button as component

--- components/MyFooter.vue
+++ components/MyFooter.vue
@@ -2,16 +2,7 @@
   <footer class="mt-4">
     <b-row no-gutters class="pb-5 pt-3 mx-5 border-top">
       <b-col>
-        <a href="https://ko-fi.com/O5O542WQX" target="_blank" rel="noopener">
-          <b-img
-            height="36"
-            style="border: 0; height: 36px"
-            src="https://cdn.ko-fi.com/cdn/kofi1.png?v=2"
-            alt="Buy Me a Coffee at ko-fi.com"
-            class="d-inline"
-            fluid
-          />
-        </a>
+        <KofiButton />
       </b-col>
       <b-col>
         <p class="text-right text-muted">
